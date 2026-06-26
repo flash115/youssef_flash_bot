b325859ab6ea0548dd46e0dcb96ba7ced7eaef34
flash115 <ahmed115adel@gmail.com>
2026-06-26 12:33:48 +0300

    Create tempmail.py

---
+++ services/tempmail.py
@@ -0,0 +1,17 @@
+import aiohttp
+
+BASE_URL = "https://api.mail.tm"
+
+class TempMail:
+
+    async def create_account(self):
+        pass
+
+    async def get_messages(self):
+        pass
+
+    async def get_message(self):
+        pass
+
+    async def delete_account(self):
+        pass
