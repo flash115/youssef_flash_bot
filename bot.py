2928c230b6d216efc13d2c8753585b08fc6e9240
flash115 <ahmed115adel@gmail.com>
2026-06-26 12:52:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
+from database.db import create_tables
 from handlers.start import router
 
 bot = Bot(BOT_TOKEN)
@@ -11,6 +12,7 @@ dp = Dispatcher()
 dp.include_router(router)
 
 async def main():
+    create_tables()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
